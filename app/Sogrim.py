e06b34352a25e4b78480a1896b0a3a2618dfc189
Marc Robin Gruener <gmarcrobin@outlook.de>
2022-05-23 01:46:59 +0200

    Adding bin size

--- app/Sogrim.py
+++ app/Sogrim.py
@@ -1,4 +1,5 @@
 from calendar import c
+from cmath import sqrt
 import streamlit as st
 import pandas as pd
 import plotly.figure_factory as ff
@@ -65,7 +66,7 @@ if nav == "Data Exploration":
   col4.metric("q3", str(all_data[choice_data_exp].quantile(q=0.75).round(2))+" "+get_data_unit(choice_data_exp))
   col5.metric("Max", str(all_data[choice_data_exp].max().round(2))+" "+get_data_unit(choice_data_exp))
 
-  fig = px.histogram(all_data[choice_data_exp])
+  fig = px.histogram(all_data[choice_data_exp], nbins=sqrt(len(all_data[choice_data_exp])))
   st.plotly_chart(fig, use_container_width=True)
 
   st.dataframe(all_data)
