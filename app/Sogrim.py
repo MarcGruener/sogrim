593c4836723679c547799b1bba261a3329450e2e
Marc Robin Gruener <gmarcrobin@outlook.de>
2022-05-23 05:41:41 +0200

    Fixed Map

--- app/Sogrim.py
+++ app/Sogrim.py
@@ -137,7 +137,7 @@ elif nav == "Location Optimizer":
   #   counties = json.load(response)
 
   with urlopen('https://datahub.io/cividi/ch-municipalities/r/gemeinden-geojson.geojson') as response:
-    st.json(response)
+    st.write(response)
 
   gemeinden = load_GeoJSON()
   st.json(gemeinden)
