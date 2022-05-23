9a979416df03ba4d3c5f5f8501a57aadc6977132
Marc Robin Gruener <gmarcrobin@outlook.de>
2022-05-23 08:24:43 +0200

    Different Maps

--- app/Sogrim.py
+++ app/Sogrim.py
@@ -95,15 +95,15 @@ elif nav == "Model Performance":
 elif nav == "Location Optimizer":
   predictions = load_predictions()
   col1, col2 = st.columns(2)
-  choice_model = col1.selectbox("Select a Model", list(predictions.drop(["GMDNAME", "lat", "lon"]).columns))
+  choice_model = col1.selectbox("Select a Model", list(predictions.drop(["GMDNAME", "lat", "lon"], axis=1).columns))
   choice_option = col2.selectbox("Select a Group", ("Consolidation", "Perfect", "Opportunities"))
 
-  # if choice_option == "Consolidation":
-  #   location_data = predictions[predictions[choice_model] < predictions.ANZAHL_FILIALEN_MIGROS]
-  # elif choice_option == "Perfect":
-  #   location_data = predictions[predictions[choice_model] == predictions.ANZAHL_FILIALEN_MIGROS]
-  # elif choice_option == "Opportunities":
-  #   location_data = predictions[predictions[choice_model] > predictions.ANZAHL_FILIALEN_MIGROS]
+  if choice_option == "Consolidation":
+    location_data = predictions[predictions[choice_model] < predictions.ANZAHL_FILIALEN_MIGROS]
+  elif choice_option == "Perfect":
+    location_data = predictions[predictions[choice_model] == predictions.ANZAHL_FILIALEN_MIGROS]
+  elif choice_option == "Opportunities":
+    location_data = predictions[predictions[choice_model] > predictions.ANZAHL_FILIALEN_MIGROS]
   
-  # st.map(location_data[[choice_model,"lat", "lon"]])
+  st.map(location_data[[choice_model,"lat", "lon"]])
 
