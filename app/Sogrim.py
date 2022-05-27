588379a57142330d49e347f8aa3d6118fc29735e
Marc Robin Gruener <gmarcrobin@outlook.de>
2022-05-27 14:25:13 +0200

    Test Geopandas

--- app/Sogrim.py
+++ app/Sogrim.py
@@ -152,7 +152,7 @@ elif nav == "Location Optimizer":
 elif nav == "TEST":
   gpd_geojson = load_geojson()
   df1 = load_predictions()
-  geo_df1 = gpd.GeoDataFrame.from_features(gpd_geojson["features"]).merge(df, left_on="gemeinde.NAME", right_on="GMDNAME").set_index("GMDNAME")
+  geo_df1 = gpd.GeoDataFrame.from_features(gpd_geojson["features"]).merge(df1, left_on="gemeinde.NAME", right_on="GMDNAME").set_index("GMDNAME")
   st.write(geo_df1)
 
   df = px.data.election()
