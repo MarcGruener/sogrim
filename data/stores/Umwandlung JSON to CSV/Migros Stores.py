876996ffe102f27527db4812ba72bb0802114b5f
Marc Robin Gruener <gmarcrobin@outlook.de>
2022-05-29 23:35:11 +0200

    Cleaning up main branch

--- data/stores/Umwandlung JSON to CSV/Migros Stores.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-with open('migros_stroes.json', encoding='utf-8') as inputfile:
-    df = pd.read_json(inputfile)
-
-#df = df.transpose(copy = True)
-
-df.to_csv('Migros Stores.csv', encoding='utf-8', index=False)
